ad9ad6ea79c66784cb6059fe7035244d42717dfe
Jiarui-Qin <scyjq5@nottingham.edu.cn>
2026-07-01 15:12:53 +0800

    Add files via upload

--- src/my_ortools/my_ortools/TSP_baseline.py
+++ src/my_ortools/my_ortools/TSP_baseline.py
@@ -1,4 +1,4 @@
-from ortools.linear_solver import pywraplp
+from ortools.comstraint_solver import routing_enums_pb2, pywrapcp
 
 def create_data_model():
     """Stores the data for the problem."""
@@ -28,10 +28,31 @@ manager = pywrapcp.RoutingIndexManager(
 routing = pywrapcp.RoutingModel(manager)
 def distance_callback(from_index, to_index):
     """Returns the distance between the two nodes."""
-    # Convert from routing variable Index to distance matrix NodeIndex.
+
     from_node = manager.IndexToNode(from_index)
     to_node = manager.IndexToNode(to_index)
     return data["distance_matrix"][from_node][to_node]
 
 transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+search_parameters.first_solution_strategy = (
+    routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+)
+def print_solution(manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()} miles")
+    index = routing.Start(0)
+    plan_output = "Route for vehicle 0:\n"
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += f" {manager.IndexToNode(index)} ->"
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += f" {manager.IndexToNode(index)}\n"
+    print(plan_output)
+    plan_output += f"Route distance: {route_distance}miles\n"
+solution = routing.SolveWithParameters(search_parameters)
+if solution:
+    print_solution(manager, routing, solution)
